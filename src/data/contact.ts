f06309bd906c101b60bd3b387d1b948633eb864e
Anurag-07-hub <anurag07hub@gmail.com>
2026-05-03 01:09:27 +0530

    admin: update contact info [auto]

---
+++ src/data/contact.ts
@@ -0,0 +1,17 @@
+import type { ContactInfo } from '../context/CMSContext';
+
+export const defaultContact: ContactInfo = {
+  "email": "endeavourinsliet@gmail.com",
+  "phone": "",
+  "address": "IIC, T&P BLOCK, SLIET, LONGOWAL, PUNJAB,148106",
+  "contacts": [
+    {
+      "name": "Anmol Ranjan",
+      "phone": "+916201957167"
+    },
+    {
+      "name": "Ashutosh Mehta",
+      "phone": "+919027042638"
+    }
+  ]
+};
