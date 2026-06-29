dde016bb2c3409e9742917229649db73bb0fe107
Anurag-07-hub <anurag07hub@gmail.com>
2026-06-29 20:04:15 +0530

    admin: update 3d model parameters [auto]

---
+++ src/data/model3d.ts
@@ -0,0 +1,15 @@
+import type { Model3DConfig } from '../context/CMSContext';
+
+export const defaultModel3D: Model3DConfig = {
+  "scale": 3.4,
+  "position": [
+    0.3,
+    -0.3,
+    0
+  ],
+  "rotation": [
+    0.02,
+    0.89,
+    0.02
+  ]
+};
