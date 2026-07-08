3792980c315f78e02480e51ed0fc790f24975969
Anurag-07-hub <anurag07hub@gmail.com>
2026-07-09 00:17:16 +0530

    admin: update hero layout [auto]

--- src/data/heroLayout.ts
+++ src/data/heroLayout.ts
@@ -54,8 +54,8 @@ export const defaultHeroLayout: HeroLayoutConfig = {
     "text": "WO"
   },
   "statementBox": {
-    "x": 172.5001220703125,
-    "y": -66.66664123535156,
+    "x": 173.33349609375,
+    "y": -74.99995422363281,
     "scale": 0.5499999999999999,
     "line1": "I BREAK THINGS",
     "line2": "TO SEE WHAT",
